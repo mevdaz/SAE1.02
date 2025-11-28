1e3afd45abeaa1434c3998817b29616935603aeb
JbMp6 <darkxyann@gmail.com>
2025-11-28 10:47:37 +0100

    Start

---
+++ src/KMPAlgo.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+class KMPAlgo {
+    void principal() {
+        
+
+    }
+
+    ArrayList<Integer> KMPSearch(ArrayList<Character> pat, String txt) {
+        
+        ArrayList<Integer> result = new ArrayList<>();
+
+        return result;
+    }
+
+    void testKmpAlgo() {
+        
+    }
+
+    void testCasKmpAlgo() {
+        
+    }
+
+    void testKmpAlgoEfficiency() {
+        
+    }
+}
