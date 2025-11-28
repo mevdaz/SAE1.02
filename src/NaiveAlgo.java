0786ffd69458d19922bc2f096def32f6848e3a49
mevdaz <leandresanders5@gmail.com>
2025-11-28 11:05:09 +0100

    feat: Implement naive string matching algorithm and text generation utility.

--- src/NaiveAlgo.java
+++ src/NaiveAlgo.java
@@ -2,17 +2,32 @@ import java.util.ArrayList;
 
 class NaiveAlgo {
     void principal() {
-
+        ArrayList<Character> text = generateText(100000);
+        System.out.println(text);
+        String pattern = "toto";
+        ArrayList<Integer> tab = naiveAlgo(text, pattern);
+        System.out.println(tab);
     }
 
-    void naiveAlgo() {
-        String pattern = "";
-        String text = "";
+    ArrayList<Character> generateText(int size) {
+        char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        ArrayList<Character> text = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            text.add(alphabet[(int) (Math.random() * alphabet.length)]);
+        }
+
+        return text;
+    }
+
+    ArrayList<Integer> naiveAlgo(ArrayList<Character> text, String pattern) {
         ArrayList<Integer> tab = new ArrayList<>();
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == pattern.charAt(0)) {
+        for (int i = 0; i < text.size(); i++) {
+            if (text.get(i) == pattern.charAt(0)) {
                 int j = 1;
-                while (j < pattern.length() && text.charAt(i + j) == pattern.charAt(j)) {
+                while (j < pattern.length() && text.get(i + j) == pattern.charAt(j)) {
                     j++;
                 }
                 if (j == pattern.length()) {
@@ -20,6 +35,7 @@ class NaiveAlgo {
                 }
             }
         }
+        return tab;
     }
 
 }
